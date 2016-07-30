90891f522d5ffcfec31be23238883ba334994f32
TanmayGarg <tanmaygarg.dev@gmail.com>
2016-07-30 12:44:09 +0530

    Added json file for database details. Now the program does not need hardcoded MongoDB details to initialise connection with the server.

--- database/database.go
+++ database/database.go
@@ -3,15 +3,48 @@ package database
 import(
   "gopkg.in/mgo.v2"
   //"gopkg.in/mgo.v2/bson"
+  "encoding/json"
+  "os"
 )
 
 var DatabaseName string
 var DatabaseSession *mgo.Session
 
-func InitDatabaseSession(url,dbname string) error{
+type DatabaseDetails struct{
+  url string `json:"url"`
+  dbname string `json:"dbname"`
+}
+
+type InitDatabaseError int
+
+func (err InitDatabaseError) Error() string{
+  if err == PROBLEMOPENINGFILE{
+    return "Could not open file uafmconfigs/dbconfig.json"
+  }
+  return "Miscellaneous Issues in database"
+}
+
+const(
+  PROBLEMOPENINGFILE = 0
+  PROBLEMDECODING = 1
+)
+
+func InitDatabaseSession() error{
   var err error
-  DatabaseSession,err = mgo.Dial(url)
+
+  myFile, err := os.Open("uafmconfigs/dbconfig.json")
+  defer myFile.Close()
+
+  if err != nil{
+    return InitDatabaseError(PROBLEMOPENINGFILE)
+  }
+  var myDBDetails DatabaseDetails
+  err = json.NewDecoder(myFile).Decode(&myDBDetails)
+  if err != nil{
+    return InitDatabaseError(PROBLEMDECODING)
+  }
+  DatabaseSession,err = mgo.Dial(myDBDetails.url)
   //defer DatabaseSession.Close().
-  DatabaseName = dbname
-  return err
+  DatabaseName = myDBDetails.dbname
+  return nil
 }
